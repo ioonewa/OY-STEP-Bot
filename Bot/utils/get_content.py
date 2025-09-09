760c84b51d604d670cc47692f33053c348e5d07f
ioonewa <ioonewa0@gmail.com>
2025-09-10 02:07:53 +0300

    update

--- Bot/utils/get_content.py
+++ Bot/utils/get_content.py
@@ -3,6 +3,8 @@ from typing import List
 
 from .stash import stash_img
 
+from .templates_rules import rules
+
 import os
 
 def overlay_photo(
@@ -12,58 +14,44 @@ def overlay_photo(
         rect_height: int,
         frame_path: str,
         photo_path: str,
-        out_file:str,
-        angle: float = 0,
-        corner_radius: int = 0
+        mask_path: str= None,   # PNG маска той же формы
+        out_file: str= "out_file.png",
+        angle: float = 0
     ):
-    # Загружаем рамку и фото
+    # Загружаем
     frame = Image.open(frame_path).convert("RGBA")
     photo = Image.open(photo_path).convert("RGBA")
+    if mask_path:
+        mask = Image.open(mask_path).convert("L")  # ч/б маска
+    else:
+        mask = None
 
-    # Масштабирование с сохранением пропорций, заполнение всего прямоугольника
+    # Масштабируем фото под размер маски
     scale_w = rect_width / photo.width
     scale_h = rect_height / photo.height
     scale = max(scale_w, scale_h)  # берём максимум, чтобы заполнить весь прямоугольник
     new_size = (int(photo.width * scale), int(photo.height * scale))
-    photo_resized = photo.resize(new_size, Image.LANCZOS)
+    photo = photo.resize(new_size, Image.LANCZOS)
 
     # Обрезка лишнего, чтобы вписать ровно в прямоугольник
-    left_crop = (photo_resized.width - rect_width) // 2
-    top_crop = (photo_resized.height - rect_height) // 2
-    photo_cropped = photo_resized.crop((
+    left_crop = (photo.width - rect_width) // 2
+    top_crop = (photo.height - rect_height) // 2
+    photo = photo.crop((
         left_crop,
         top_crop,
         left_crop + rect_width,
         top_crop + rect_height
     ))
 
-    # Поворот (expand=True, чтобы не обрезались углы)
-    photo_rotated = photo_cropped.rotate(angle, expand=True)
-
-
-    # --- СОЗДАНИЕ МАСКИ ДЛЯ СКРУГЛЕНИЯ ---
-    mask = Image.new("L", photo_rotated.size, 0)  # ч/б маска
-    draw = ImageDraw.Draw(mask)
-
-    if corner_radius > 0:
-        # Скруглённый прямоугольник
-        draw.rounded_rectangle(
-            (0, 0, photo_rotated.width, photo_rotated.height),
-            radius=corner_radius,
-            fill=255
-        )
-    else:
-        # Без маски (прямоугольник)
-        mask = None
-
-    # Если есть маска → применяем
+    # Поворачиваем (если нужно)
+    photo = photo.rotate(angle, expand=True)
+    # Добавляем альфу по маске
     if mask:
-        photo_rotated.putalpha(mask)
+        mask = mask.resize(photo.size, Image.LANCZOS)
+        photo.putalpha(mask)
 
-    # Вставка фото в рамку
-    frame.paste(photo_rotated, (left, top), photo_rotated)
-
-    # Сохранение
+    # Вставляем в рамку
+    frame.paste(photo, (left, top), photo)
     frame.save(out_file)
     return out_file
 
@@ -108,302 +96,6 @@ def overlay_text(
     frame.save(file_path)
 
     return file_path
-
-rules = {
-    "2": {
-        "post": {
-            "font": {
-                "name": "TTChocolates-Bold.ttf",
-                "size": 40,
-                "upper": True
-            },
-            "photo": {
-                "top": 189,
-                "left": 93,
-                "rect_width": 894,
-                "rect_height": 540
-            },
-            "text": {
-                "name": {
-                    "text_x": 218,
-                    "text_y": 970
-                },
-                "phone_number": {
-                    "text_x": 218,
-                    "text_y": 1020
-                },
-                "email": {
-                    "text_x": 218,
-                    "text_y": 1070
-                },
-                "username": {
-                    "text_x": 218,
-                    "text_y": 1120
-                }
-            }
-        },
-        "story": {
-            "font": {
-                "name": "TTChocolates-Bold.ttf",
-                "size": 56,
-                "upper": True
-            },
-            "photo": {
-                "top": 240,
-                "left": 73,
-                "rect_width": 934,
-                "rect_height": 720
-            },
-            "text": {
-                "name": {
-                    "text_x": 220,
-                    "text_y": 1324
-                },
-                "phone_number": {
-                    "text_x": 220,
-                    "text_y": 1384
-                },
-                "email": {
-                    "text_x": 220,
-                    "text_y": 1444
-                },
-                "username": {
-                    "text_x": 220,
-                    "text_y": 1504
-                }
-                
-            }
-        },
-        "video": {},
-        "text_colors": {
-            "Black": (255,255,255,255),
-            "Color": (255,255,255,255),
-            "Pastel": (0,0,0,255),
-            "White": (0,0,0,255)
-        }
-    },
-    "3": {
-        "post": {
-            "font": {
-                "name": "TTChocolates-Regular.ttf",
-                "size": 42,
-                "upper": True,
-                "center": False,
-                "offset_x": 20
-            },
-            "photo": {
-                "top": 109,
-                "left": 196,
-                "rect_width": 605,
-                "rect_height": 600,
-                "angle": 5
-            },
-            "text": {
-                "name": {
-                    "text_x": 309,
-                    "text_y": 843
-                },
-                "phone_number": {
-                    "text_x": 309,
-                    "text_y": 890
-                },
-                "email": {
-                    "text_x": 309,
-                    "text_y": 935
-                },
-                "username": {
-                    "text_x": 309,
-                    "text_y": 985
-                }
-            }
-        },
-        "story": {
-            "font": {
-                "upper": True,
-                "name": "TTChocolates-Regular.ttf",
-                "size": 44
-            },
-            "photo": {
-                "top": 236,
-                "left": 148,
-                "rect_width": 745,
-                "rect_height": 736,
-                "angle": 5,
-            },
-            "text": {
-                "name": {
-                    "text_x": 320,
-                    "text_y": 1232
-                },
-                "phone_number": {
-                    "text_x": 320,
-                    "text_y": 1280
-                },
-                "email": {
-                    "text_x": 320,
-                    "text_y": 1325
-                },
-                "username": {
-                    "text_x": 320,
-                    "text_y": 1370
-                }
-            },
-        },
-        "video": {},
-        "text_colors": {
-            "Black": (235,50,35,255),
-            "Color": (235,50,35,255),
-            "Pastel": (235,50,35,255),
-            "White": (235,50,35,255)
-        }
-    },
-    "4": {
-        "post": {
-            "font": {
-                "name": "TTChocolates-Regular.ttf",
-                "size": 58,
-                "center": True
-            },
-            "photo": {
-                "top": 292,
-                "left": 110,
-                "rect_width": 860,
-                "rect_height": 530
-            },
-            "text": {
-                "name": {
-                    "text_x": 224,
-                    "text_y": 900
-                },
-                "phone_number": {
-                    "text_x": 224,
-                    "text_y": 970
-                },
-                "email": {
-                    "text_x": 224,
-                    "text_y": 1040
-                },
-                "username": {
-                    "text_x": 224,
-                    "text_y": 1110
-                },
-            }
-        },
-        "story": {
-            "font": {
-                "name": "TTChocolates-Regular.ttf",
-                "size": 62,
-                "center": True
-            },
-            "photo": {
-                "top": 577,
-                "left": 109,
-                "rect_width": 862,
-                "rect_height": 532
-            },
-            "text": {
-                "name": {
-                    "text_x": 160,
-                    "text_y": 1225
-                },
-                "phone_number": {
-                    "text_x": 160,
-                    "text_y": 1315
-                },
-                "email": {
-                    "text_x": 160,
-                    "text_y": 1405
-                },
-                "username": {
-                    "text_x": 160,
-                    "text_y": 1495
-                },
-                
-            },
-        },
-        "video": {},
-        "text_colors": {
-            "Black": (255,255,255,255),
-            "Color": (255,255,255,255),
-            "Pastel": (255,255,255,255),
-            "White": (0,0,0,255)
-        }
-    },
-    "5": {
-        "post": {
-            "font": {
-                "name": "TTChocolates-Regular.ttf",
-                "size": 58,
-                "center": True
-            },
-            "photo": {
-                "top": 58,
-                "left": 140,
-                "rect_width": 800,
-                "rect_height": 800,
-                "corner_radius": 10000
-            },
-            "text": {
-                "name": {
-                    "text_x": 224,
-                    "text_y": 1000
-                },
-                "phone_number": {
-                    "text_x": 224,
-                    "text_y": 1070
-                },
-                "email": {
-                    "text_x": 224,
-                    "text_y": 1140
-                },
-                "username": {
-                    "text_x": 224,
-                    "text_y": 1210
-                },
-            }
-        },
-        "story": {
-            "font": {
-                "name": "TTChocolates-Regular.ttf",
-                "size": 62,
-                "center": True
-            },
-            "photo": {
-                "top": 290,
-                "left": 110,
-                "rect_width": 860,
-                "rect_height": 860,
-                "corner_radius": 1000
-            },
-            "text": {
-                "name": {
-                    "text_x": 160,
-                    "text_y": 1325
-                },
-                "phone_number": {
-                    "text_x": 160,
-                    "text_y": 1415
-                },
-                "email": {
-                    "text_x": 160,
-                    "text_y": 1505
-                },
-                "username": {
-                    "text_x": 160,
-                    "text_y": 1595
-                },
-                
-            },
-        },
-        "video": {},
-        "text_colors": {
-            "Black": (255,255,255,255),
-            "Color": (0,0,0,255),
-            "Pastel": (0,0,0,255),
-            "White": (0,0,0,255)
-        }
-    }
-}
 
 def get_user_preview(
         photo_path: str,
---
+++ Bot/utils/templates_rules.py
@@ -0,0 +1,367 @@
+rules = {
+    "2": {
+        "post": {
+            "font": {
+                "name": "TTChocolates-Bold.ttf",
+                "size": 40,
+                "upper": True
+            },
+            "photo": {
+                "top": 189,
+                "left": 182,
+                "rect_width": 720,
+                "rect_height": 540
+            },
+            "text": {
+                "name": {
+                    "text_x": 218,
+                    "text_y": 970
+                },
+                "phone_number": {
+                    "text_x": 218,
+                    "text_y": 1020
+                },
+                "email": {
+                    "text_x": 218,
+                    "text_y": 1070
+                },
+                "username": {
+                    "text_x": 218,
+                    "text_y": 1120
+                }
+            }
+        },
+        "story": {
+            "font": {
+                "name": "TTChocolates-Bold.ttf",
+                "size": 56,
+                "upper": True
+            },
+            "photo": {
+                "top": 240,
+                "left": 180,
+                "rect_width": 720,
+                "rect_height": 720
+            },
+            "text": {
+                "name": {
+                    "text_x": 220,
+                    "text_y": 1324
+                },
+                "phone_number": {
+                    "text_x": 220,
+                    "text_y": 1384
+                },
+                "email": {
+                    "text_x": 220,
+                    "text_y": 1444
+                },
+                "username": {
+                    "text_x": 220,
+                    "text_y": 1504
+                }
+                
+            }
+        },
+        "video": {},
+        "text_colors": {
+            "Black": (255,255,255,255),
+            "Color": (255,255,255,255),
+            "Pastel": (0,0,0,255),
+            "White": (0,0,0,255)
+        }
+    },
+    "3": {
+        "post": {
+            "font": {
+                "name": "TTChocolates-Regular.ttf",
+                "size": 42,
+                "center": False,
+                "offset_x": 20
+            },
+            "photo": {
+                "top": 109,
+                "left": 196,
+                "rect_width": 605,
+                "rect_height": 600,
+                "angle": 5
+            },
+            "text": {
+                "name": {
+                    "text_x": 309,
+                    "text_y": 843
+                },
+                "phone_number": {
+                    "text_x": 309,
+                    "text_y": 890
+                },
+                "email": {
+                    "text_x": 309,
+                    "text_y": 935
+                },
+                "username": {
+                    "text_x": 309,
+                    "text_y": 985
+                }
+            }
+        },
+        "story": {
+            "font": {
+                "name": "TTChocolates-Regular.ttf",
+                "size": 44
+            },
+            "photo": {
+                "top": 236,
+                "left": 148,
+                "rect_width": 745,
+                "rect_height": 736,
+                "angle": 5,
+            },
+            "text": {
+                "name": {
+                    "text_x": 320,
+                    "text_y": 1232
+                },
+                "phone_number": {
+                    "text_x": 320,
+                    "text_y": 1280
+                },
+                "email": {
+                    "text_x": 320,
+                    "text_y": 1325
+                },
+                "username": {
+                    "text_x": 320,
+                    "text_y": 1370
+                }
+            },
+        },
+        "video": {},
+        "text_colors": {
+            "Black": (235,50,35,255),
+            "Color": (235,50,35,255),
+            "Pastel": (235,50,35,255),
+            "White": (235,50,35,255)
+        }
+    },
+    "4": {
+        "post": {
+            "font": {
+                "name": "TTChocolates-Regular.ttf",
+                "size": 58,
+                "center": True
+            },
+            "photo": {
+                "top": 292,
+                "left": 110,
+                "rect_width": 860,
+                "rect_height": 530
+            },
+            "text": {
+                "name": {
+                    "text_x": 224,
+                    "text_y": 900
+                },
+                "phone_number": {
+                    "text_x": 224,
+                    "text_y": 970
+                },
+                "email": {
+                    "text_x": 224,
+                    "text_y": 1040
+                },
+                "username": {
+                    "text_x": 224,
+                    "text_y": 1110
+                },
+            }
+        },
+        "story": {
+            "font": {
+                "name": "TTChocolates-Regular.ttf",
+                "size": 62,
+                "center": True
+            },
+            "photo": {
+                "top": 577,
+                "left": 109,
+                "rect_width": 862,
+                "rect_height": 532
+            },
+            "text": {
+                "name": {
+                    "text_x": 160,
+                    "text_y": 1225
+                },
+                "phone_number": {
+                    "text_x": 160,
+                    "text_y": 1315
+                },
+                "email": {
+                    "text_x": 160,
+                    "text_y": 1405
+                },
+                "username": {
+                    "text_x": 160,
+                    "text_y": 1495
+                },
+                
+            },
+        },
+        "video": {},
+        "text_colors": {
+            "Black": (255,255,255,255),
+            "Color": (255,255,255,255),
+            "Pastel": (255,255,255,255),
+            "White": (0,0,0,255)
+        }
+    },
+    "5": {
+        "post": {
+            "font": {
+                "name": "TTChocolates-Regular.ttf",
+                "size": 58,
+                "center": True
+            },
+            "photo": {
+                "top": 58,
+                "left": 140,
+                "rect_width": 800,
+                "rect_height": 800,
+                "mask_path": "content/templates/5/post_mask.png"
+            },
+            "text": {
+                "name": {
+                    "text_x": 224,
+                    "text_y": 1000
+                },
+                "phone_number": {
+                    "text_x": 224,
+                    "text_y": 1070
+                },
+                "email": {
+                    "text_x": 224,
+                    "text_y": 1140
+                },
+                "username": {
+                    "text_x": 224,
+                    "text_y": 1210
+                },
+            }
+        },
+        "story": {
+            "font": {
+                "name": "TTChocolates-Regular.ttf",
+                "size": 62,
+                "center": True
+            },
+            "photo": {
+                "top": 300,
+                "left": 120,
+                "rect_width": 840,
+                "rect_height": 840,
+                "mask_path": "content/templates/5/story_mask.png"
+            },
+            "text": {
+                "name": {
+                    "text_x": 160,
+                    "text_y": 1325
+                },
+                "phone_number": {
+                    "text_x": 160,
+                    "text_y": 1415
+                },
+                "email": {
+                    "text_x": 160,
+                    "text_y": 1505
+                },
+                "username": {
+                    "text_x": 160,
+                    "text_y": 1595
+                },
+                
+            },
+        },
+        "video": {},
+        "text_colors": {
+            "Black": (255,255,255,255),
+            "Color": (0,0,0,255),
+            "Pastel": (0,0,0,255),
+            "White": (0,0,0,255)
+        }
+    },
+    "6": {
+        "post": {
+            "font": {
+                "name": "TTChocolates-Regular.ttf",
+                "size": 58,
+                "center": True
+            },
+            "photo": {
+                "top": 334,
+                "left": 0,
+                "rect_width": 1080,
+                "rect_height": 652,
+                "mask_path": "content/templates/6/post_mask.png"
+            },
+            "text": {
+                "name": {
+                    "text_x": 224,
+                    "text_y": 1020
+                },
+                "phone_number": {
+                    "text_x": 224,
+                    "text_y": 1090
+                },
+                "email": {
+                    "text_x": 224,
+                    "text_y": 1160
+                },
+                "username": {
+                    "text_x": 224,
+                    "text_y": 1230
+                },
+            }
+        },
+        "story": {
+            "font": {
+                "name": "TTChocolates-Regular.ttf",
+                "size": 62,
+                "center": True
+            },
+            "photo": {
+                "top": 478,
+                "left": 0,
+                "rect_width": 1080,
+                "rect_height": 924,
+                "mask_path": "content/templates/6/story_mask.png"
+            },
+            "text": {
+                "name": {
+                    "text_x": 160,
+                    "text_y": 1470
+                },
+                "phone_number": {
+                    "text_x": 160,
+                    "text_y": 1560
+                },
+                "email": {
+                    "text_x": 160,
+                    "text_y": 1650
+                },
+                "username": {
+                    "text_x": 160,
+                    "text_y": 1740
+                },
+                
+            },
+        },
+        "video": {},
+        "text_colors": {
+            "Black": (0,0,0,255),
+            "Color": (0,0,0,255),
+            "Pastel": (0,0,0,255),
+            "White": (0,0,0,255)
+        }
+    }
+}
