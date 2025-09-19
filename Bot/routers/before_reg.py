ecc978ab5216c3c5c714637e5c80eaf656f51d6f
ioonewa <ioonewa0@gmail.com>
2025-09-19 11:46:11 +0300

    fix

--- Bot/routers/before_reg.py
+++ Bot/routers/before_reg.py
@@ -5,15 +5,15 @@ from aiogram.types import Message
 router = Router()
 
 
-@router.message(Command("/info"))
+@router.message(Command("info"))
 async def payment_data(message: Message):
     await message.answer(
         "<b>OYSTEP | Офис брокера.</b>\n\n"
-        "Тел: +79998200319"
         "Наши услуги и цены:\n"
         "\t\t\t\t⭐️ Подписка Стандарт — 5000 ₽/мес\n\n"
         """<i>ИП Мудрак Максим Владимирович
 ИНН 230603988280
 ОГРН 325237500344922
+Тел: +79998200319
 Адрес:  350067, Краснодарский край, Краснодар, ул. Гаражная, 79/1, кв. 45</i>"""
     )
