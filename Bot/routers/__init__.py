6893bd0fc127eb42c121cd44bd64ccd9228d8adf
ioonewa <ioonewa0@gmail.com>
2025-09-19 11:40:52 +0300

    add info block

--- Bot/routers/__init__.py
+++ Bot/routers/__init__.py
@@ -1,6 +1,7 @@
 from .post_content import router as post_content_router
 from .interface import router as interface_router
 from .registration import router as reg_router
+from .before_reg import router as before_reg_router
 
 from .settings import router as settings_router
 
@@ -11,6 +12,7 @@ for r in [post_content_router, interface_router, settings_router]:
     r.callback_query.filter(RegFilter())
 
 routers = [
+    before_reg_router,
     post_content_router,
     settings_router,
     interface_router,
